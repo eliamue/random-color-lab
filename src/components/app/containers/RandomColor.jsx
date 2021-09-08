e738014b6bd29e78eec37a95161331f0045af3b1
Elise Muellerleile <eliamue@live.com>
2021-09-08 13:04:54 -0500

    bonus success baby works

--- src/components/app/containers/RandomColor.jsx
+++ src/components/app/containers/RandomColor.jsx
@@ -8,24 +8,35 @@ const bgcolor = [
   'green',
   'blue',
   '#330099',
-  'violet'
+  'violet',
 ];
 
 export default class RandomColor extends Component {
   state = {
-    bgcolor: ''
-  }
+    bgcolor: '',
+  };
 
   changeColor = () => {
-    const rando =  Math.floor(Math.random() * bgcolor.length);
-    this.setState({ bgcolor: bgcolor[rando] });
-  }
+    const rando = Math.floor(Math.random() * bgcolor.length);
+    if(bgcolor[rando] === this.state.bgcolor) {
+      this.setState({
+        bgcolor:
+          'url(https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg)',
+      });
+    } else {
+      this.setState({ bgcolor: bgcolor[rando] });
+    }
+  };
 
   componentDidMount() {
     setInterval(() => this.changeColor(), 1000);
   }
 
   render() {
-    return <div><Display bgcolor={this.state.bgcolor}/></div>;
+    return (
+      <div>
+        <Display bgcolor={this.state.bgcolor} />
+      </div>
+    );
   }
 }
