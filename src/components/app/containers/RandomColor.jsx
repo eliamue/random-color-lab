8523635460e631085b9d76c52630e9d2ecf19f65
Elise Muellerleile <eliamue@live.com>
2021-09-07 21:36:36 -0500

    colors randomly generate

--- src/components/app/containers/RandomColor.jsx
+++ src/components/app/containers/RandomColor.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import Display from '../../Display.jsx';
 
+const bgcolor = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  '#330099',
+  'violet'
+];
+
 export default class RandomColor extends Component {
+  state = {
+    bgcolor: ''
+  }
+
+  changeColor = () => {
+    const rando =  Math.floor(Math.random() * bgcolor.length);
+    this.setState({ bgcolor: bgcolor[rando] });
+  }
+
+  componentDidMount() {
+    setInterval(() => this.changeColor(), 1000);
+  }
+
   render() {
-    return <div></div>;
+    return <div><Display bgcolor={this.state.bgcolor}/></div>;
   }
 }
