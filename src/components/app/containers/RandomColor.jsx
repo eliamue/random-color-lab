d9c629a98af38b2521ad73edfad417c7f054556a
Elise Muellerleile <eliamue@live.com>
2021-09-07 20:33:17 -0500

    init

---
+++ src/components/app/containers/RandomColor.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Display from '../../Display.jsx';
+
+export default class RandomColor extends Component {
+  render() {
+    return <div></div>;
+  }
+}
